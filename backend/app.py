e7cd28dc098b61020bed873a88f7a7f62c9ae96f
poojana <poojana1608@gmail.com>
2026-09-01 18:11:51 +0530

    Prepare backend for deployment

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify
 from flask_cors import CORS
 
@@ -18,4 +20,10 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
